e81be578b792f314498e9bfd4644e4e558ac7fe0
Morten Dall <mortendall1988@gmail.com>
2022-12-08 15:16:09 +0100

    updated targets pipeline

--- _targets.R
+++ _targets.R
@@ -9,7 +9,7 @@ library(targets)
 
 # Set target options:
 tar_option_set(
-  packages = c("tibble"), # packages that your targets need to run
+    packages = desc::desc_get_deps()$package[-1], # packages that your targets need to run
   format = "rds" # default storage format
   # Set other options as needed.
 )
@@ -26,13 +26,17 @@ tar_source()
 
 # Replace the target list below with your own:
 list(
-  tar_target(
-    name = data,
-    command = tibble(x = rnorm(100), y = rnorm(100))
-#   format = "feather" # efficient storage of large data frames # nolint
-  ),
-  tar_target(
-    name = model,
-    command = coefficients(lm(y ~ x, data = data))
-  )
+    tar_target(
+        name = file,
+        command = "data/lipidomics.csv",
+        format = "file"
+    ),
+    tar_target(
+        name = lipidomics,
+        command = readr::read_csv(file, show_col_types = FALSE)
+    ),
+    tar_target(
+        name = df_stats_by_metabolite,
+        command = descriptive_stats(lipidomics)
+    )
 )
